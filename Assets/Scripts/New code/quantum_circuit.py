71e87c5839c5f4480f01c560f20a6ab632b06a2c
teerachat1090 <teerachat.kh@gmail.com>
2026-03-07 01:22:34 +0700

    edit python file
    make python can calulate for both Bloch-sphere (single qubit) and Q-sphere (multiple qubit)

--- Assets/Scripts/New code/quantum_circuit.py
+++ Assets/Scripts/New code/quantum_circuit.py
@@ -5,6 +5,8 @@ import sys
 import traceback
 import json
 from itertools import zip_longest
+
+import numpy as np
 from qiskit import QuantumCircuit
 from qiskit.quantum_info import Statevector
 
@@ -14,24 +16,24 @@ from qiskit.quantum_info import Statevector
 
 # at esic: py->python, lenovo->esicl
 # cd "Assets/Scripts/New code"
-# py ./sample.py "C:\Users\lenovo\AppData\LocalLow\DefaultCompany\VR quantum\QuantumData\QuantumInput\bloch_circuit_input.json"
+# py ./quantum_circuit.py "C:\Users\lenovo\AppData\LocalLow\DefaultCompany\VR quantum\QuantumData\QuantumInput\bloch_circuit_input.json"
 # "C:\Users\lenovo\AppData\LocalLow\DefaultCompany\VR quantum\QuantumData\QuantumOutput\bloch_circuit_output.json"
 
-# single_input_gate[<key>](qc)(qubit)
+# single_input_gate[<key>](qc)(qubit_amount)
 # qc = QuantumCircuit(n)
-# qubit -> position of qubit
+# qubit_amount -> position of qubit_amount
 single_input_gate = {
-    "I":lambda qc, qubit : qc.id(qubit),
-    "H":lambda qc, qubit : qc.h(qubit),
-    "X":lambda qc, qubit : qc.x(qubit),
-    "Y":lambda qc, qubit : qc.y(qubit),
-    "Z":lambda qc, qubit : qc.z(qubit),
-    "S":lambda qc, qubit : qc.s(qubit),
-    "ST":lambda qc, qubit : qc.sdg(qubit), #S-dagger
-    "T":lambda qc, qubit : qc.t(qubit),
-    "TT":lambda qc, qubit : qc.tdg(qubit), #T-dagger
-    "SQRTX":lambda qc, qubit : qc.sx(qubit),
-    "SQRTXT":lambda qc, qubit : qc.sxdg(qubit), #square root x - dagger
+    "I":lambda qc, qubit_amount : qc.id(qubit_amount),
+    "H":lambda qc, qubit_amount : qc.h(qubit_amount),
+    "X":lambda qc, qubit_amount : qc.x(qubit_amount),
+    "Y":lambda qc, qubit_amount : qc.y(qubit_amount),
+    "Z":lambda qc, qubit_amount : qc.z(qubit_amount),
+    "S":lambda qc, qubit_amount : qc.s(qubit_amount),
+    "ST":lambda qc, qubit_amount : qc.sdg(qubit_amount), #S-dagger
+    "T":lambda qc, qubit_amount : qc.t(qubit_amount),
+    "TT":lambda qc, qubit_amount : qc.tdg(qubit_amount), #T-dagger
+    "SQRTX":lambda qc, qubit_amount : qc.sx(qubit_amount),
+    "SQRTXT":lambda qc, qubit_amount : qc.sxdg(qubit_amount), #square root x - dagger
 }
 
 # get json data
@@ -43,41 +45,41 @@ def load_circuit_from_json(json_path: str):
 #use only single input gate
 def create_bloch_sphere_circuit(circuit_data):
     """sample text"""
-    qubit = circuit_data.get("qubitAmount", None)
-    qubits = circuit_data.get("qubits", None)
-    
-    if qubit is None:
-        print("Json file error: no field name (qubit - int)!")
+    qubit_amount = circuit_data.get("qubitAmount", None)
+    qubit_array = circuit_data.get("qubits", None)
+
+    if qubit_amount is None:
+        print("Json file error: no field name (qubit_amount - int)!")
         sys.exit(1)
 
-    if qubits is None:
-        print("Json file error: no field name (qubits - array)!")
+    if qubit_array is None:
+        print("Json file error: no field name (qubit_array - array)!")
         sys.exit(1)
 
-    gates = qubits[0].get("gateList", None)
-    qubit_list = [q.get("gateList", None) for q in qubits]
+    gates = qubit_array[0].get("gateList", None)
+    qubit_amount_list = [q.get("gateList", None) for q in qubit_array]
 
 
     if gates is None:
         print("Json file error: no field name (gateList - array)!")
         sys.exit(1)
 
-    qc = QuantumCircuit(qubit)
-    
-    
+    qc = QuantumCircuit(qubit_amount)
 
-    for gate in gates:
-        gate_type = str(gate.get("gateName", None)).strip().upper()
-        if gate_type is None:
-            continue
+    for each_column in zip_longest(*qubit_amount_list, fillvalue=None):
+        for index, each_row in enumerate(each_column):
+            gate_type = str(each_row.get("gateName", None)).strip().upper()
 
-        if gate_type in single_input_gate:
-            single_input_gate[gate_type](qc,qubit-1)
+            if gate_type == "":
+                continue
+
+            if gate_type in single_input_gate:
+                single_input_gate[gate_type](qc,index)
 
     return qc
 
 # New issue: make new case for Q-Sphere
-def build_result_json(is_bloch_sphere : bool, qc : QuantumCircuit):
+def build_result_json(qc : QuantumCircuit):
     """Get output of the circuit and put result to dictionary """
 
     current_datetime = datetime.datetime.now()
@@ -90,9 +92,13 @@ def build_result_json(is_bloch_sphere : bool, qc : QuantumCircuit):
 
     states = Statevector.from_instruction(qc)
     probs = states.probabilities()
+    phase = np.angle(states.data, deg=True)
 
     for i, state in enumerate(states):
-        entry = {"value":i, "real_part":state.real, "imag_part":state.imag, "prob":probs[i]}
+        entry = {
+            "value":i, "real_part":state.real, "imag_part":state.imag, 
+            "phase":phase[i], "prob":probs[i]
+        }
         data["state"].append(entry)
 
 
@@ -118,15 +124,12 @@ def main():
             print("Json file error: no field name (blochSphere - bool)!")
             sys.exit(1)
 
-        if is_bloch_sphere is True :
-            qc = create_bloch_sphere_circuit(circuit_data)
-            result_json = build_result_json(is_bloch_sphere, qc)
+        qc = create_bloch_sphere_circuit(circuit_data)
+        result_json = build_result_json(qc)
 
-            with open(json_output_path, "w", encoding="utf-8") as file:
-                json.dump(result_json, file, indent=4)
-        else:
-            #for Q-sphere
-            print("for Q-sphere")
+        with open(json_output_path, "w", encoding="utf-8") as file:
+            json.dump(result_json, file, indent=4)
+   
     except FileNotFoundError:
         traceback.print_exc(file=sys.stderr)
         sys.exit(1)
